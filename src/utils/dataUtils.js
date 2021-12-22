1aee9041c9ecd8cbcd5f7bd28630b14d8d658750
Janne Räsänen <raskitjjj@gmail.com>
2021-12-22 11:08:00 +0200

    Fixed filter to actually pump out values instead of undefined

--- src/utils/dataUtils.js
+++ src/utils/dataUtils.js
@@ -1,38 +1,40 @@
 /**
  * Filters out all other datapoints except the ones closes to 00:00:00 UTC
- * @param {Object} {prices, market_caps, total_volumes}-object from
+ * @param {Object} CoinData {prices, market_caps, total_volumes}-object from
  *                 coingecko api.
  * @returns Datapoints at 00:00:00 UTC
  */
 export const filterDays = ({ prices, market_caps, total_volumes }) => {
   const granularity = getGranularity(prices);
-  if (granularity == 1) return { prices, market_caps, total_volumes };
+  if (granularity === 1) return { prices, market_caps, total_volumes };
 
   const newArrayLength = Math.floor(prices.length / granularity);
 
   return {
-    prices: [
-      ...Array(newArrayLength).map(({}, index) => prices[index * granularity]),
-    ],
-    market_caps: [
-      ...Array(newArrayLength).map(({}, index) => market_caps[index * granularity]),
-    ],
-    total_volumes: [
-      ...Array(newArrayLength).map(({}, index) => total_volumes[index * granularity]),
-    ],
+    prices: [...Array(newArrayLength)].map(
+      (item, index) => prices[index * granularity]
+    ),
+    market_caps: [...Array(newArrayLength)].map(
+      (item, index) => market_caps[index * granularity]
+    ),
+    total_volumes: [...Array(newArrayLength)].map(
+      (item, index) => total_volumes[index * granularity]
+    ),
   };
 };
 
 /**
  * Returns how many data points are between the daily data points.
- * @param {Object} prices {[Timestamps], [Prices]}-object from coingecko api.
+ * @param {Object} timestampValuePair {[Timestamps], [Prices]}-object from coingecko api.
  * @returns Interval value used to determine which points corresponds to daily
  *          datapoints at 00:00:00 UTC
  */
-const getGranularity = ({ prices }) => {
-  if (prices.length < 2) return 1;
+const getGranularity = (timestampValuePair) => {
+  if (!timestampValuePair || timestampValuePair.length < 2) return 1;
   // Calculate the time difference between data points so we only calculate trend for days.
-  const timeDiff = Math.floor((prices[1][0] - prices[0][0]) * 0.001);
+  const timeDiff = Math.floor(
+    (timestampValuePair[1][0] - timestampValuePair[0][0]) * 0.001
+  );
   if (timeDiff < 6000) {
     if (timeDiff < 1500) {
       return 288;
